d48597c52e723f019cd543c03e544deb2586a959
pirog <alice.may.01010@gmail.com>
2020-08-06 20:22:23 +0300

    MVC second part

--- seaGame.js
+++ seaGame.js
@@ -13,65 +13,90 @@ let view = {
     }
 };
 
+
 let model = {
     boardSize: 7,  //размер игрового поля
     numShips: 3,   //кол-во кораблей
     shipLength: 3, //длинна корабля
-    shipsSome: 0,  //потопленные корабли
+    shipsSunk: 0,  //потопленные корабли
     ships: [
-        ship1 = { location: ['10', '20', '30'], hits: ['','',''] },
-        ship2 = { location: ['32', '33', '34'], hits: ['','',''] },
-        ship3 = { location: ['63', '64', '65'], hits: ['','',''] }
+        ship1 = { location: ['10', '20', '30'], hits: ['', '', ''] },
+        ship2 = { location: ['32', '33', '34'], hits: ['', '', ''] },
+        ship3 = { location: ['63', '64', '65'], hits: ['', '', ''] }
     ],
 
     fire: function (guess) {  //получаем координаты выстрела
         for (let i = 0; i < this.numShips; i++) {
             let ship = this.ships[i];
-            location = ship.location;
-            let index = location.indexOf(guess);
+            let index = ship.location.indexOf(guess);
             if (index >= 0) {
                 ship.hits[index] = 'hit';
+                view.displayHit(guess);
+                view.displayMessage('Вы попали!');
+                if (this.isSunk(ship)) {
+                    view.displayMessage('Корабль потоплен!');
+                    this.shipsSunk++;
+                }
                 return true;
             }
         }
+        view.displayMiss(guess);
+        view.displayMessage('Вы промазали!');
         return false;
+    },
+
+    isSunk: function (ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== 'hit') {
+                return false;
+            }
+        }
+        return true;
     }
-    
+
 };
 
 
+let controller = {
+    guesses: 0,
 
-/*let location1 = Math.floor(Math.random() * 5);
-let location2 = location1 + 1;messageArea
-let location3 = location2 + 1;
-
-let guess;
-let hits = 0;
-let guesses = 0;
-
-let isSunk = false;
-
-while (isSunk == false) {
-    guess = prompt('Готов выстрелить? (введи цифру 0-6):');
-    if (guess < 0 || guess > 6) {
-        alert('Число от 0 до 6!!!');
-    }
-    else {
-        guesses = guesses + 1;
-        if (guess == location1 || guess == location2 || guess == location3) {
-            hits = hits + 1;
-            if (hits == 3) {
-                isSunk = true;
-                alert('Ты победил!');
-
+    processGuess: function (guess) {
+        let location = rightGuesses(guess);
+        if (location) {
+            this.guesses++;
+            let hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage('Победа! Вы потопили ' + model.numShips + ' кораблей за ' + this.guesses + ' выстрелов.');
             }
-        }
-        else {
-            alert('Мимо!');
         }
     }
 }
-alert('Вы выстрелили ' + guesses + ' раз!');*/
+
+function rightGuesses(guess) {
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+    if (guess === null || guess.length !== 2) {
+        alert('Вы ввели неверные координаты!');
+    } else {
+        let firstChar = guess.charAt(0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt(1);
+        if (isNaN(row) || isNaN(column)) {
+            alert('Вы ввели неверные координаты!');
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert('Вы ввели неверные координаты!');
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
+
+/*let location1 = Math.floor(Math.random() * 5);
+let location2 = location1 + 1;messageArea
+let location3 = location2 + 1;*/
+
+
 
 
 
